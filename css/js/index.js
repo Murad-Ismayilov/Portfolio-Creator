a80f9fe075989424a72b1aab21aab9e0271aad45
Murad İsmayilov <mismayilov429@gmail.com>
2024-03-22 18:18:11 +0400

    S5

---
+++ css/js/index.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+   
+  });
+
+
